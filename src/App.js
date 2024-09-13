a562f887cc921537a7aa733adeae6baf2469a626
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-13 16:30:22 +0300

    router dosyası temel yapı oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import AppRouter from "./router/AppRouter";
+
 function App() {
   return (
     <div>
+      <AppRouter />
     </div>
   );
 }
---
+++ src/router/AppRouter.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+            
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
