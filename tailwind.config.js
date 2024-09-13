f458007383a9ac56eb930f43d3080e7afb7e6541
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-13 16:25:08 +0300

    kullanılacak renk paleti ve font stilleri tailwind dosyasına eklendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "primary-color": "#FABC3F",
+      "secondary-color": "#E85C0D",
+      "light-red": "#C7253E",
+      "dark-red": "#821131",
+      "font-color": "#E9EFEC",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
