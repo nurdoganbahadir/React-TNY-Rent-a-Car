0e6e911682817577225e41b6165000962a2a1a6e
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-16 19:15:41 +0300

    login stili ayarlandı

--- src/pages/ManagePanel.jsx
+++ src/pages/ManagePanel.jsx
@@ -1,9 +1,51 @@
-
-
 const ManagePanel = () => {
   return (
-    <div>ManagePanel</div>
-  )
-}
+    <div className="w-[300px] m-auto my-5 p-5 rounded-[20px] ">
+      <div className="flex gap-2">
+        <span className="w-[6px] h-[39px] bg-secondary-color"></span>
+        <h1 className="text-center font-serif text-3xl">
+          TNY Rent a Car Login
+        </h1>
+      </div>
 
-export default ManagePanel
+      <form className="flex flex-col text-left p-3 gap-5">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email" className="font-serif text-xl">
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="rounded-[10px]"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="font-serif text-xl">
+            Password
+          </label>
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="rounded-[10px]"
+            required
+          />
+        </div>
+        <button className="bg-light-red h-[44px] font-serif text-label uppercase hover:opacity-90 rounded-[4px] text-white">
+          Sign In
+        </button>
+        <div className="flex justify-center items-center flex-wrap">
+          <span className="text-label font-montserrat font-[500] text-labelColor">
+            Forgot your password?
+          </span>
+          <span className="text-main text-[14px] font-montserrat font-[500] underline ml-1 ">
+            Reset password
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ManagePanel;
