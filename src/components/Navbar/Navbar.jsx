ec7205b5381a9a43b1c8b05c51cdb53cbfd38301
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-16 19:22:03 +0300

    login sayfası stil ayarlandı

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav class="bg-white dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <nav class="bg-gray-300 dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img
@@ -78,7 +78,7 @@ const Navbar = () => {
             class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
                   to="/"
--- src/pages/ManagePanel.jsx
+++ src/pages/ManagePanel.jsx
@@ -3,14 +3,14 @@ const ManagePanel = () => {
     <div className="w-[300px] m-auto my-5 p-5 rounded-[20px] ">
       <div className="flex gap-2">
         <span className="w-[6px] h-[39px] bg-secondary-color"></span>
-        <h1 className="text-center font-serif text-3xl">
+        <h1 className="text-center font-serif text-3xl dark:text-white">
           TNY Rent a Car Login
         </h1>
       </div>
 
       <form className="flex flex-col text-left p-3 gap-5">
         <div className="flex flex-col gap-1">
-          <label htmlFor="email" className="font-serif text-xl">
+          <label htmlFor="email" className="font-serif text-xl dark:text-white">
             Email
           </label>
           <input
@@ -21,7 +21,10 @@ const ManagePanel = () => {
           />
         </div>
         <div className="flex flex-col gap-1">
-          <label htmlFor="password" className="font-serif text-xl">
+          <label
+            htmlFor="password"
+            className="font-serif text-xl dark:text-white"
+          >
             Password
           </label>
 
@@ -36,10 +39,10 @@ const ManagePanel = () => {
           Sign In
         </button>
         <div className="flex justify-center items-center flex-wrap">
-          <span className="text-label font-montserrat font-[500] text-labelColor">
+          <span className="text-label font-montserrat font-[500] text-labelColor dark:text-white">
             Forgot your password?
           </span>
-          <span className="text-main text-[14px] font-montserrat font-[500] underline ml-1 ">
+          <span className="text-main text-[14px] font-montserrat font-[500] underline ml-1 dark:text-white">
             Reset password
           </span>
         </div>
