eb2197fd57053a655c501b80141a7a5ed6640da2
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-16 14:01:19 +0300

    error sayfası ayarlandı

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,11 @@
+import errorlogo from "../../assets/images/error.png";
+
+const Error = () => {
+  return (
+    <div>
+      <img src={errorlogo} alt="error" className="w-[100%] h-[50vh]" />
+    </div>
+  );
+};
+
+export default Error;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Home from "../pages/Home";
+import Error from "../components/Error/Error";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
