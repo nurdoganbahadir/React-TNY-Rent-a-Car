c56c7c37199da774d616ab0cb19b39bfb5e15c45
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-18 13:14:07 +0300

    card'ların dark temadaki style yapıları ayarlandı

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ const Home = () => {
   return (
     <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8">
       <div className="text-center">
-        <h1 className="text-3xl text-gray-800 font-semibold">
+        <h1 className="text-3xl text-gray-800 font-semibold dark:text-white">
           Hoşgeldiniz, araçlarımıza bir göz atmaya ne dersiniz?
         </h1>
-        <p className="mt-3 text-gray-500">
+        <p className="mt-3 text-gray-500 dark:text-gray-300">
           Araçlarımız her zaman hazır ve nazır bir şekilde siz değerli
           müşterilerimizi beklemektedir.
         </p>
@@ -27,10 +27,10 @@ const Home = () => {
                 alt={items.id}
                 className="w-full h-48 rounded-t-md"
               />
-              <div className="flex justify-between items-center mt-2 pt-3 ml-4 mr-2">
+              <div className="flex justify-between items-center mt-2 pt-3 ml-4 mr-2 dark:text-white">
                 <div className="">Günlük {items.price}₺</div>
                 <div className="ml-3">
-                  <span className="block text-gray-900 float-end">
+                  <span className="block text-gray-900 float-end dark:text-white">
                     {items.year}
                   </span>
                   <span className="block text-gray-400 text-sm">
@@ -39,10 +39,12 @@ const Home = () => {
                 </div>
               </div>
               <div className="pt-3 ml-4 mr-2 mb-3">
-                <h3 className="text-xl text-gray-900 uppercase">
+                <h3 className="text-xl text-gray-900 uppercase dark:text-white">
                   {items.brand}
                 </h3>
-                <p className="text-gray-400 text-sm mt-1">{items.model}</p>
+                <p className="text-gray-400 text-sm mt-1 capitalize">
+                  {items.model}
+                </p>
               </div>
             </a>
           </article>
