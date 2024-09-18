ab2da73f2937149533cfbcd67b639b12a5c45a82
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-19 00:16:35 +0300

    home sayfasına slider eklendi

--- src/components/Other/MySwiper.jsx
+++ src/components/Other/MySwiper.jsx
@@ -16,23 +16,27 @@ const MySwiper = () => {
     },
   };
   return (
-    <div className="h-[50vh]">
-      <Swiper
-        pagination={pagination}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img src={logo} alt="" /></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </div>
+    
+      <div className="h-[40vh] mb-16">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <img src={logo} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={logo} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={logo} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={logo} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={logo} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    
   );
 };
 
