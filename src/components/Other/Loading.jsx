b294e2e0a6af655c95e3d093f7f71850f649d18e
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-25 16:50:19 +0300

    react-loading-indicators eklendi ve loading component tamamlandı

--- src/components/Other/Loading.jsx
+++ src/components/Other/Loading.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { BlinkBlur } from "react-loading-indicators";
 
 const Loading = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="text-center my-12">
+      <BlinkBlur color="#31cc4c" size="medium" text="" textColor="#66f4ba" />
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
